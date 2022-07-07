523ec2a13c406e9d20b74db77d8d16bd2f4ba41e
Shubham Kukreti <shubhamkukreti204@gmail.com>
2022-07-08 01:36:52 +0530

    added: hero and navbar

---
+++ components/Hero/hero2.js
@@ -0,0 +1,11 @@
+export default function Hero2() {
+    return (
+    <div>
+        <div className="relative">
+            <div className="">
+
+            </div>
+        </div>
+    </div>
+    )
+  }
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Navbar from '../components/Global/navbar'
+import Hero from '../components/Hero/hero'
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className="bg-black">
+      <Navbar />
+      <Hero />
     </div>
   )
 }
